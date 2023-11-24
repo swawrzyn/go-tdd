0fc4607baa3f91658832a886d4139f53995edc3e
Stefan Wawrzyn <swawrzyn@gmail.com>
2023-11-24 15:47:21 +0800

    chore: finish 01-hello_world

--- 01-hello_world/hello.go
+++ 01-hello_world/hello.go
@@ -2,10 +2,32 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name + "!"
+const englishPrefix = "Hello"
+const polishPrefix = "Witam"
+const chinesePrefix = "你好"
+
+func greetingPrefix(lang string) (prefix string) {
+
+	switch lang {
+	case "polish":
+		prefix = polishPrefix
+	case "中文":
+		prefix = chinesePrefix
+	default:
+		prefix = englishPrefix
+	}
+
+	return
+}
+
+func Hello(name string, lang string) string {
+	if name == "" {
+		name = "world"
+	}
+
+	return greetingPrefix(lang) + ", " + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello("Stefan"))
+	fmt.Println(Hello("Stefan", "english"))
 }
--- 01-hello_world/hello_test.go
+++ 01-hello_world/hello_test.go
@@ -3,9 +3,34 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Stefan")
-	want := "Hello, Stefan!"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Stefan", "english")
+		want := "Hello, Stefan!"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, world!' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "english")
+		want := "Hello, world!"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Polish", func(t *testing.T) {
+		got := Hello("Marek", "polish")
+		want := "Witam, Marek!"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in 中文", func(t *testing.T) {
+		got := Hello("李锐", "中文")
+		want := "你好, 李锐!"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
